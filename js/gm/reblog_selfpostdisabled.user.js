c3a93df8eeaf80f7faf8e74ebaec5cc4c8e1ebd2
sugizou <osugi.to.pico@gmail.com>
2009-06-09 03:35:08 +0900

    練習がてらに書いてみた。tumblrのdashboardで自分のpostを消す(作りかけ)

---
+++ js/gm/reblog_selfpostdisabled.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name           Reblog Self Post Disabled
+// @namespace      http://sugizou.jugem.jp/
+// @include        http://www.tumblr.com/*
+// @exclude        
+// ==/UserScript==
+
+(function() {
+  base = "/html/body[@id='dashboard_index']/div[@id='container']/div[@id='content']/div[@id='left_column']/ol[@id='posts']/li";
+  res = document.evaluate(base, document, null, 7, null);
+	    
+  cnt = 0;
+  do {
+    field = res.snapshotItem(cnt);
+    id    = field.getAttribute('id');
+		chk   = field.getAttribute('class');
+
+		reg   = new RegExp('is_mine');
+		if(id != null && chk.match(reg)) {
+      path = base + "[@id='" + id + "']";
+		  docs = document.evaluate(path, document, null, 7, null);
+		  target = docs.snapshotItem(0);
+		  target.innerHTML = '<span style="color: text;">block!</span>';
+		}
+		cnt++;
+  } while(res.snapshotItem(cnt) != null);
+}());
+// 
